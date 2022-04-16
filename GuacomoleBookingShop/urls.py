a46e720f36046971b97092e2b6c63fd6571091d8
Karel-Kryšpín Kratochvíl <xkratoc8@mendelu.cz>
2022-04-16 18:29:05 +0200

    Commit 1. Přidání Users a připravování pro db, 2. commit vytváření configu (složka mainBlog)

--- GuacomoleBookingShop/urls.py
+++ GuacomoleBookingShop/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views
+from django.contrib.auth import views as auth_view
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls'))
 ]
